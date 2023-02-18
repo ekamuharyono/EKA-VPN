ff2b84a83563b62b41219e10f5ace8d9f322f4bb
root <ekamuharyono02@gmail.com>
2023-02-18 09:32:47 +0700

    Base Setup Bot Telegram

---
+++ app.js
@@ -0,0 +1,84 @@
+import TelegramBot from "node-telegram-bot-api"
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import { responStart } from './responses.js' // import file berisi responses
+
+const token = process.env.TELEGRAM_BOT_TOKEN  // ambil token bot telegram dari .env
+
+const bot = new TelegramBot(token, { polling: true }) // initialize bot telegram
+
+// MAIN CODE
+bot.on('message', (msg) => {
+
+    const chatId = msg.chat.id // id telegram user
+
+    //  HANDLE COMMAND START
+    if (msg.text.includes('/start')) {
+
+        return bot.sendMessage(chatId, responStart()) // Show help info
+
+    }
+
+    // HANDLE COMMAND CEK
+    if (msg.text.includes('/cek')) {
+
+        // cek user memasukan username atau tidak
+        if (msg.text.length > 4) {
+
+            const message = msg.text.split(' ')
+            let input = message[1].split('.') // data yg diinput user
+
+            // jika user memasukan data
+            return bot.sendMessage(chatId, `Silahkan Tunggu sedang mencari user ${input}`)
+
+            // validasi data yg di inputkan terdaftar atau tidak
+
+            // jika user ditemukan
+
+            // jika user tidak ditemukan
+
+        } else {
+
+            // Jika user tidak memasukan username
+            bot.sendMessage(chatId, 'Anda tidak memasukan user dan password')
+
+        }
+
+    }
+
+    // HANDLE COMMAND CREATE
+    if (msg.text.includes('/create')) {
+
+        // cek user memasukan data atau tidak
+        if (msg.text.length > 7) {
+
+            const message = msg.text.split(' ')
+            let input = message[1].split('.') // data yg diinput user
+
+            const user = input[0] // username yg di input
+            const pass = input[1] // password yg di input
+
+            // jika user memasukan data cek data lengkap atau tidak
+            if (user && pass) {
+
+                // jika data lengkap, lakukan validasi
+                return bot.sendMessage(chatId, `Silahkan Tunggu ... `)
+
+            } else {
+
+                // jika user memasukan data tidak lengkap
+                return bot.sendMessage(chatId, "Masukan data dengan format yang benar")
+
+            }
+
+        } else {
+
+            // JIka user tidak memasukan data input
+            return bot.sendMessage(chatId, 'Anda tidak memasukan username dan password')
+
+        }
+
+    }
+
+})
---
+++ responses.js
@@ -0,0 +1,24 @@
+export const responStart = () => {
+  return `
+🙏 Selamat Datang di VPN EKA 🙏
+(Ethernet Keep Alive)
+  
+Panduan lengkap VPN Remote Gratis : http://bit.ly/gratis-vpn-remote
+          
+  Buat VPN :
+  /create username.password
+  Contoh : /create bambang.1234
+   
+  Cek List Port VPN :
+  /cek username
+  Contoh : /cek bambang
+      
+Bisa langganan harian, mingguan, bulanan !!!
+VPN Remote Gratis | VPN Game ML Gratis | 
+VPN Sosmed | VPN Trafik | VPN Game | 
+Mikhmon Online | Jasa Setting | Konsultasi
+Open Reseller!!!
+   
+📞 WA : wa.me/6282153393216
+🍹 Donasi : saweria.co/ekamhryn`
+}
